dee4e588f53a252cd7b6c5c85e7bcd19cc1d39ab
if6100 <116769034+if6100@users.noreply.github.com>
2024-11-07 17:41:13 -0600

    Add files via upload

---
+++ TipoTriangulo.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package cr.go.ucr.triangulo;
+
+/**
+ *
+ * @author JeArag
+ */
+public enum TipoTriangulo {
+    NO_TRIANGULO("NT"),ESCALENO("ES"),EQUILATERO("EQ"),ISOCELES("IS");
+    private String nombre;
+    private TipoTriangulo(String nombre){this.nombre=nombre;}
+
+    public String getNombre() {
+        return nombre;
+    }
+    
+    
+}
