8654ae296daa4184c57c42764d0d27d00b148567
jearag <jaragonca@ice.go.cr>
2024-11-14 19:02:16 -0600

    Codigo del cambio/ fecha archivos explicacion

--- src/main/java/cr/go/ucr/triangulo/TipoTriangulo.java
+++ src/main/java/cr/go/ucr/triangulo/TipoTriangulo.java
@@ -9,7 +9,7 @@ package cr.go.ucr.triangulo;
  * @author JeArag
  */
 public enum TipoTriangulo {
-    NO_TRIANGULO("NT"),ESCALENO("ES"),EQUILATERO("EQ"),ISOCELES("IS");
+    NO_TRIANGULO("NT"),ESCALENO("ES"),EQUILATERO("EQ"),ISOCELES("IS"),TIPO_3("tipo3");
     private String nombre;
     private TipoTriangulo(String nombre){this.nombre=nombre;}
 
